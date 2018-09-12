0fd65096198703aba4e2721cad2eeca80e8ad654
toilari <ilari97@hotmail.com>
2018-09-12 11:19:20 +0300

    Minor cleanup Some leftovers from tests with XKBlib and Xlib-xcb

--- include/darkhouse.hpp
+++ include/darkhouse.hpp
@@ -1,17 +1,13 @@
 #pragma once
 
-#include <X11/Xlib.h>
-
 #include "config.hpp"
-#include "window.hpp"
 
 namespace darkhouse {
+class Window;
 class Darkhouse {
   private:
-    Display *m_xlib_display;
     class xcb_connection_t *m_connection;
     class xcb_screen_t *m_screen;
-
     Window *m_window;
 
   public:
